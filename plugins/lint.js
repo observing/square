edab2d08083d61f2309fb0f2565f789d3e1dde34
3rd-Eden <info@3rd-Eden.com>
2012-04-19 12:16:12 +0200

    Decent linter reporting

--- plugins/lint.js
+++ plugins/lint.js
@@ -19,7 +19,17 @@ module.exports = function setup (options) {
    */
 
   return function linting (output, next) {
+    var config = this.package.configuration;
 
+    // make sure that we have a parser for this extension
+    if (!(output.extension in parsers)) return next();
+
+    parsers[output.extension](output.content, config, function linted (err, failures) {
+      if (err) return next(err);
+      if (failures && failures.length) reporters.base(output, failures);
+
+      next();
+    });
   };
 };
 
@@ -36,10 +46,10 @@ var parsers = {
       canihas.jshint(function lazyload (err, jshint) {
         if (err) return fn(err);
 
-        var validates = jshint(content, options)
+        var validates = jshint.JSHINT(content, options)
           , errors;
 
-        if (!validates) errors = formatters.js(jshint.errors);
+        if (!validates) errors = formatters.js(jshint.JSHINT.errors);
         fn(null, errors);
       });
     }
@@ -57,7 +67,7 @@ var parsers = {
       canihas.csslint(function lazyload (err, csslint) {
         if (err) return fn(err);
 
-        var validates = csslint.verify(content, options)
+        var validates = csslint.CSSLint.verify(content, options)
           , errors;
 
         if (validates.messages.length) errors = formatters.css(validates.messages);
@@ -85,8 +95,9 @@ var formatters = {
       return fail.map(function oops (err) {
         return {
             line: err.line
-          , column: err.characture
+          , column: err.character
           , message: err.reason
+          , ref: err
         };
       });
     }
@@ -105,7 +116,87 @@ var formatters = {
             line: err.line
           , column: err.col
           , message: err.message
+          , ref: err
         };
       });
     }
 };
+
+/**
+ * Output reports.
+ *
+ * @api private
+ */
+
+var reporters = {
+    base: function (file, errors, options) {
+      var reports = []
+        , content = file.content.split('\n');
+
+      errors.forEach(function error (err) {
+        // some linters don't return the location -_-
+        if (!err.line) return reports.push(err.message.grey, '');
+
+        var start = err.line > 2 ? err.line - 2 : 0
+          , stop = err.line + 2
+          , range = content.slice(start, stop)
+          , numbers = _.range(start + 1, stop + 1)
+          , len = stop.toString().length;
+
+        reports.push('Lint error: ' + err.line + ' col ' + err.column);
+        range.map(function (line) {
+          var lineno = numbers.shift()
+            , offender = lineno === err.line
+            , inline = /\'[^\']+?\'/
+            , slice;
+
+          // this is the actual line with the error, so we should start finding
+          // what the error is and how we could highlight it in the output
+          if (offender) {
+            if (line.length < err.column) {
+              // we are missing something at the end of the line.. so add a red
+              // square
+              line += ' '.inverse.red;
+            } else {
+              // we have a direct match on a statement
+              if (inline.test(err.message)) {
+                slice = err.message.match(inline)[0].replace(/\'/g, '');
+              } else {
+                // it's happening in the center of things, so we can start
+                // coloring inside the shizzle
+                slice = line.slice(err.column - 1);
+              }
+
+              line = line.replace(slice, slice.inverse.red);
+            }
+          }
+
+          reports.push('  ' + pad(lineno, len) + ' | ' + line);
+        });
+
+        reports.push('');
+        reports.push(err.message.grey);
+        reports.push('');
+
+      });
+
+      // output the shizzle
+      reports.forEach(function output (line) {
+        console.error(line);
+      });
+    }
+};
+
+/**
+ * Pad a string
+ *
+ * @param {String} str
+ * @param {Number} len
+ * @returns {String}
+ * @api private
+ */
+
+function pad (str, len) {
+  str = '' + str;
+  return new Array(len - str.length + 1).join(' ') + str;
+}
