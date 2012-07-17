4cb768143e6feb50fc5cd34b1b69a05ffee611ee
3rd-Eden <info@3rd-Eden.com>
2012-07-17 22:24:40 +0200

    remove test, re-do the whole test suite, it sucked balls

--- tests/fixtures/basic.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function foo () { return 'pew'; }
-function bar () { return 'wep'; }
-
-var varbar = 'bar';
-var varfoo = 'foo';
--- tests/fixtures/cupof.coffee
+++
@@ -1,9 +0,0 @@
-# Assignment:
-number   = 42
-opposite = true
-
-# Conditions:
-number = -42 if opposite
-
-# Functions:
-square = (x) -> x * x
