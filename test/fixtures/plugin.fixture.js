1216cb5d0da4b3442062cfea5446f6ce99d5d5db
3rd-Eden <info@3rd-Eden.com>
2012-07-24 11:11:10 +0200

    Started work on new fixtures so we can test out middleware#forEach functions

--- test/fixtures/plugin.fixture.js
+++ test/fixtures/plugin.fixture.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 /**
  * Simple helper fixture that helps us with testing the plugin system.
  *
@@ -8,12 +10,19 @@
 module.exports = function fixture(options) {
   "use strict";
 
-  var square = this;
+  var square = this
+    , settings = {
+        'throw': false
+      , 'no-content': false
+    };
+
   square.emit('plugin.fixture:init', options, this);
+  _.extend(settings, options || {});
 
   return function middleware(output, next) {
     square.emit('plugin.fixture:call', output, this);
 
+    if (settings['throw'] === true) throw new Error('throwing an error');
     next();
   };
 };
