532bafaaeb5095f865b9dabc77bf1c88ee69d186
3rd-Eden <info@3rd-Eden.com>
2012-07-28 23:12:52 +0200

    square#forEach
    Added unit test against the square#forEach method that iterates the different
    middleware layers. To ensure that all errors are caught and the content and it's
    failures are handled correctly.

--- test/fixtures/plugin.fixture.js
+++ test/fixtures/plugin.fixture.js
@@ -14,6 +14,8 @@ module.exports = function fixture(options) {
     , settings = {
         'throw': false
       , 'no-content': false
+      , 'change-content': false
+      , 'error-argument': false
     };
 
   square.emit('plugin.fixture:init', options, this);
@@ -22,7 +24,26 @@ module.exports = function fixture(options) {
   return function middleware(output, next) {
     square.emit('plugin.fixture:call', output, this);
 
+    // throwing does not trigger the next function, which is awesome as we can
+    // see if we can change the content
     if (settings['throw'] === true) throw new Error('throwing an error');
-    next();
+
+    // call the callback function without any content, it should just fallback
+    // to the old "backup" version of the content
+    if (settings['no-content'] === true) return next();
+
+    // change the content to something
+    if (settings['change-content'] === true) {
+      output.content = 'changed the content';
+      return next(null, output);
+    }
+
+    // call the function's with an error argument instead of throwing it
+    if (settings['error-argument'] === true) {
+      return next(new Error('error argument'));
+    }
+
+    // fallback if none of the fixtures above are tested
+    next(null, output);
   };
 };
