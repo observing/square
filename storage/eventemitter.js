51ce39fda9e51113c25b90658101c619defa948c
3rd-Eden <info@3rd-Eden.com>
2012-12-11 22:05:54 +0100

    [api] Adding support for different storage engines #33

--- storage/eventemitter.js
+++ storage/eventemitter.js
@@ -6,10 +6,8 @@
  * MIT Licensed
  */
 
-var fs = require('fs');
-
 /**
- * Write the output of the file to a directory on disk
+ * Emit the output instead of writing it.
  *
  * @param {Square} square The reference to the square instance
  * @param {Object} collection The collection and file details
@@ -17,5 +15,5 @@ var fs = require('fs');
  * @api public
  */
 module.exports = function write(square, collection, fn) {
-  fs.writeFile(collection.file, collection.content, fn);
+
 };
