0a67c2e52596beb0a67a6e9682707ec110cd9ed9
3rd-Eden <info@3rd-Eden.com>
2013-01-03 22:52:13 +0100

    [test] Working on getting the test suite working again

--- test/fixtures/plugin.fixture.js
+++ test/fixtures/plugin.fixture.js
@@ -1,4 +1,11 @@
-var _ = require('lodash');
+'use strict';
+
+/**!
+ * [square]
+ * @copyright (c) 2012 observe.it (observe.it) <opensource@observe.it>
+ * MIT Licensed
+ */
+var Plugin = require('../../plugin');
 
 /**
  * Simple helper fixture that helps us with testing the plugin system.
@@ -6,51 +13,103 @@ var _ = require('lodash');
  * @type {Function}
  * @api public
  */
+module.exports = Plugin.extend({
+    /**
+     * Name of the module.
+     *
+     * @type {String}
+     */
+    name: 'Fixture'
 
-module.exports = function fixture(options) {
-  "use strict";
+    /**
+     * Small description about the module.
+     *
+     * @type {String}
+     */
+  , description: [
+        'Simple fixture for testing the plugins integration'
+    ].join(' ')
 
-  var square = this
-    , settings = {
-        'throw': false
-      , 'no-content': false
-      , 'change-content': false
-      , 'error-argument': false
-      , 'unique-id': 0
-    };
+    /**
+     * For which distributions should this run.
+     *
+     * @type {Array}
+     */
+  , distributions: []
 
-  square.emit('plugin.fixture:init', options, this);
-  _.extend(settings, options || {});
+    /**
+     * Which file extension are accepted.
+     *
+     * @type {Array}
+     */
+  , accepts: []
 
-  return function middleware(output, next) {
-    square.emit('plugin.fixture:call', output, this);
+    /**
+     * Should we throw an Error, just for fun and giggles
+     *
+     * @type {Boolean}
+     */
+  , 'throw': false
 
-    // we need to emit an id, so we can see if the plugins are processed in the
-    // correct order
-    if (settings['unique-id'] > 0) {
-      square.emit('plugin.fixture:id', settings['unique-id']);
+    /**
+     * Should we return nothing as content?
+     *
+     * @type {Boolean}
+     */
+  , 'no-content': false
+
+    /**
+     * Should we change the content?
+     *
+     * @type {Boolean}
+     */
+  , 'change-content': false
+
+    /**
+     * Should we use an error argument.
+     *
+     * @type {Boolean}
+     */
+  , 'error-argument': false
+
+    /**
+     * An unique id.
+     *
+     * @type {Number}
+     */
+  , 'unique-id': 0
+
+    /**
+     * Initialize all the things.
+     */
+  , initialize: function initialize() {
+      this.square.emit('plugin.fixture:call', this);
+
+      // we need to emit an id, so we can see if the plugins are processed in the
+      // correct order
+      if (+this['unique-id'] > 0) {
+        this.square.emit('plugin.fixture:id', this['unique-id']);
+      }
+
+      // throwing does not trigger the next function, which is awesome as we can
+      // see if we can change the content
+      if (this['throw'] === true) throw new Error('throwing an error');
+
+      // call the callback function without any content, it should just fallback
+      // to the old "backup" version of the content
+      if (this['no-content'] === true) return this.emit('data');
+
+      // change the content to something
+      if (this['change-content'] === true) {
+        return this.emit('data', 'changed the content');
+      }
+
+      // call the function's with an error argument instead of throwing it
+      if (this['error-argument'] === true) {
+        return this.emit('error', new Error('error argument'));
+      }
+
+      // always emit something
+      this.emit('data', this.content);
     }
-
-    // throwing does not trigger the next function, which is awesome as we can
-    // see if we can change the content
-    if (settings['throw'] === true) throw new Error('throwing an error');
-
-    // call the callback function without any content, it should just fallback
-    // to the old "backup" version of the content
-    if (settings['no-content'] === true) return next();
-
-    // change the content to something
-    if (settings['change-content'] === true) {
-      output.content = 'changed the content';
-      return next(null, output);
-    }
-
-    // call the function's with an error argument instead of throwing it
-    if (settings['error-argument'] === true) {
-      return next(new Error('error argument'));
-    }
-
-    // fallback if none of the fixtures above are tested
-    next(null, output);
-  };
-};
+});
