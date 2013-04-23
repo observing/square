cc39c48e76baa1b502aa66aac25a1ed886cda883
Swaagie <info@martijnswaagman.nl>
2013-04-23 15:14:21 +0200

    [fix] moar timeout

--- test/watch.test.js
+++ test/watch.test.js
@@ -98,7 +98,7 @@ describe('[square] watch API', function () {
     var Notify, findit, square, watcher;
 
     beforeEach(function (done) {
-      this.timeout(20000);
+      this.timeout(60000);
 
       square = new Square({ 'disable log transport': true });
       square.parse(fixtures +'/read/adeptable.json');
